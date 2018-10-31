628812b6b885e6786485d027a6e0df737422f82d
Ken Hick <hick.ken@gmail.com>
2018-10-31 16:09:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -58,6 +58,8 @@ def winner(board)
   WIN_COMBINATIONS.each do |combo|
     if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
       return "X"
+    elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+      return "O"
     end
   end
 end
