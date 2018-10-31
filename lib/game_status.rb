0cdfc3121ff738a05132bc02f27a921533351468
Ken Hick <hick.ken@gmail.com>
2018-10-31 15:43:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,8 +20,9 @@ def won?(board)
     if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
       return combo
     end
-  elsif board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
+  elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
     return combo
   end
+end
   return false
 end
